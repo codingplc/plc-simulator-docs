5e5e42394ade9630037ece4d720de57d0e27fee8
Dagryl <simon@techsolid.io>
2023-06-16 19:52:29 +0100

    add user map to landing page

---
+++ components/Landing.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UsersMap from './Landing/UsersMap';
+
+export default function Landing() {
+  return (
+    <div>
+      <UsersMap />
+    </div>
+  );
+}
---
+++ components/Landing/UsersMap.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'next/image';
+import userWorldMap from '../../svg/userWorldMap.svg';
+
+export default function UsersMap() {
+  return (
+    <section>
+      <h2 className="mb-6 text-3xl font-extrabold tracking-tight text-center text-gray-900 lg:mb-8 lg:text-3xl dark:text-white">
+        Join users from 187 countries
+      </h2>
+      <p className="mb-8 font-light lg:text-xl">
+        Be part of a vibrant, worldwide network committed to mastering and innovating in the field
+        of programmable logic controllers. Come, join the journey of knowledge exchange and skill
+        enhancement with users from every corner of the world!
+      </p>
+      <Image priority src={userWorldMap} alt="User World Map" />
+    </section>
+  );
+}
