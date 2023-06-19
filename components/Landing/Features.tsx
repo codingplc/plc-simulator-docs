8855991ff88c749c9b45d08ee6cf35434c9edd08
Dagryl <simon@techsolid.io>
2023-06-19 22:33:16 +0100

    add docs file structure

--- components/Landing/Features.tsx
+++ components/Landing/Features.tsx
@@ -16,7 +16,7 @@ export default function Features() {
                 <svg
                   stroke="currentColor"
                   fill="currentColor"
-                  stroke-width="0"
+                  strokeWidth="0"
                   viewBox="0 0 24 24"
                   height="100%"
                   width="100%"
@@ -45,7 +45,7 @@ export default function Features() {
                 <svg
                   stroke="currentColor"
                   fill="currentColor"
-                  stroke-width="0"
+                  strokeWidth="0"
                   viewBox="0 0 24 24"
                   height="100%"
                   width="100%"
@@ -75,7 +75,7 @@ export default function Features() {
                 <svg
                   stroke="currentColor"
                   fill="currentColor"
-                  stroke-width="0"
+                  strokeWidth="0"
                   viewBox="0 0 24 24"
                   height="100%"
                   width="100%"
