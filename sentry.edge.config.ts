bcd5ae6315d9e26a7cbbd140b199876d6132225c
Dagryl <simon@techsolid.io>
2023-06-20 19:11:36 +0100

    sentry release

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,10 +6,11 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
+  release: 'plc-simulator-docs@' + process.env.NEXT_PUBLIC_VERSION,
   dsn: "https://47da3ec12a8a40098daa240b09c9ef1a@o4505075727990784.ingest.sentry.io/4505393048846336",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.2,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
