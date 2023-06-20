e70254d6cdc6bf186c346baed678eb8a63b9fb28
Dagryl <simon@techsolid.io>
2023-06-21 00:23:49 +0100

    static export

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+});
