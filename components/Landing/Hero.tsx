89fb505dfb875147e27803924687e5944bb502b0
Dagryl <simon@techsolid.io>
2023-07-03 14:23:29 +0100

    add run simulator link and logo

--- components/Landing/Hero.tsx
+++ components/Landing/Hero.tsx
@@ -20,21 +20,9 @@ export default function Hero() {
             href="https://app.plcsimulator.online/"
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-xl px-5 py-2.5 text-center dark:focus:ring-primary-900"
+            className="inline-flex items-center text-white bg-primary-800 hover:bg-primary-900 focus:ring-4 font-medium rounded-lg text-xl px-5 py-2.5 text-center"
           >
-            Run PLC Simulator Online
-            <svg
-              className="ml-2 -mr-1 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            Run PLC Simulator Online ↗
           </a>
         </div>
         <img
--- theme.config.tsx
+++ theme.config.tsx
@@ -5,7 +5,15 @@ import { useRouter } from 'next/router';
 const currentYear = new Date().getFullYear();
 
 const config: DocsThemeConfig = {
-  logo: <span>PLC Simulator Online</span>,
+  primaryHue: 200,
+  logo: (
+    <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path
+        fill="#1D7AD7"
+        d="m384,224.85v-96.85h-64v256h64v-95.15h128v95.15c0,70.69-57.31,128-128,128H128C57.31,512,0,454.69,0,384v-96h128v96h64V128h-64v96H0v-96C0,57.31,57.31,0,128,0h256c70.69,0,128,57.31,128,128v96.85h-128Z"
+      />
+    </svg>
+  ),
   project: {
     link: 'https://github.com/dagryl/plc-simulator-docs',
   },
@@ -32,6 +40,18 @@ const config: DocsThemeConfig = {
       />
     </>
   ),
+  navbar: {
+    extraContent: (
+      <a
+        href="https://app.plcsimulator.online/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-flex items-center text-white bg-primary-800 hover:bg-primary-900 font-medium rounded-lg text-l px-4 py-2 text-center "
+      >
+        Run Simulator
+      </a>
+    ),
+  },
   useNextSeoProps() {
     const { asPath } = useRouter();
     if (asPath !== '/') {
