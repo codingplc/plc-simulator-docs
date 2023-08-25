db8fe7152c1e17e74be0adf0cfa4deadc4e48716
Dagryl <simon@techsolid.io>
2023-08-25 19:14:00 +0100

    add Branch document

--- components/Landing/UsersMap.tsx
+++ components/Landing/UsersMap.tsx
@@ -15,7 +15,7 @@ export default function UsersMap() {
       </p>
       <Image className="mx-auto" priority src={userWorldMap} alt="User World Map" />
 
-      <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:pb-16 lg:pb-6">
+      <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:pb-4">
         <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
           <div className="flex flex-col items-center justify-center">
             <dt className="mb-2 text-3xl md:text-4xl font-extrabold">5000+</dt>
