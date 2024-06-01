39b49db7f8dee7d9cf550b768e6f554ebe2d1c01
Dagryl <simon@techsolid.io>
2024-06-01 14:50:38 +0100

    chore: update statistics

--- components/Landing/UsersMap.tsx
+++ components/Landing/UsersMap.tsx
@@ -9,24 +9,23 @@ export default function UsersMap() {
         Join users from 187 countries
       </h2>
       <p className="mb-8 font-light lg:text-xl">
-        Be part of a vibrant, worldwide network committed to mastering and innovating in the field
-        of programmable logic controllers. Come, join the journey of knowledge exchange and skill
-        enhancement with users from every corner of the world!
+        Be part of a vibrant, worldwide network committed to mastering and innovating in the field of programmable logic controllers. Come, join the journey of
+        knowledge exchange and skill enhancement with users from every corner of the world!
       </p>
       <Image className="mx-auto" priority src={userWorldMap} alt="User World Map" />
 
       <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:pb-4">
         <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
           <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl md:text-4xl font-extrabold">5000+</dt>
+            <dt className="mb-2 text-3xl md:text-4xl font-extrabold">10k+</dt>
             <dd className="font-light text-gray-500 dark:text-gray-400">registered users</dd>
           </div>
           <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl md:text-4xl font-extrabold">25k+</dt>
+            <dt className="mb-2 text-3xl md:text-4xl font-extrabold">40k+</dt>
             <dd className="font-light text-gray-500 dark:text-gray-400">saved projects</dd>
           </div>
           <div className="flex flex-col items-center justify-center">
-            <dt className="mb-2 text-3xl md:text-4xl font-extrabold">400+</dt>
+            <dt className="mb-2 text-3xl md:text-4xl font-extrabold">500+</dt>
             <dd className="font-light text-gray-500 dark:text-gray-400">users daily</dd>
           </div>
         </dl>
